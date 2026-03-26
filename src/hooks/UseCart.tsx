9e5e30830a788235672a2ff674faa6d730280e24
myrza2012 <myrza5229@gmail.com>
2026-03-26 17:38:57 +0600

    hooks buttum

--- src/hooks/UseCart.tsx
+++ src/hooks/UseCart.tsx
@@ -1,1 +1,37 @@
-// 
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+export interface ICartProduct {
+  id: number;
+  title: string;
+  price: number;
+  image: string;
+}
+
+interface CartStore {
+  cart: ICartProduct[];
+  addToCart: (item: ICartProduct) => void;
+  removeFromCart: (id: number) => void;
+  clearCart: () => void;
+}
+
+export const useCart = create<CartStore>()(
+  persist(
+    (set) => ({
+      cart: [],
+
+      addToCart: (item) =>
+        set((state) => ({
+          cart: [...state.cart, item],
+        })),
+
+      removeFromCart: (id) =>
+        set((state) => ({
+          cart: state.cart.filter((p) => p.id !== id),
+        })),
+
+      clearCart: () => set({ cart: [] }),
+    }),
+    { name: "cart" }
+  )
+);
--- src/hooks/UseProduct.tsx
+++ src/hooks/UseProduct.tsx
@@ -1,1 +1,29 @@
-//
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+export interface IProduct {
+  id: number;
+  title: string;
+  price: number;
+  image: string;
+  description?: string;
+}
+
+interface ProductStore {
+  products: IProduct[];
+  setProducts: (items: IProduct[]) => void;
+  getProductById: (id: number) => IProduct | undefined;
+}
+
+export const useProduct = create<ProductStore>()(
+  persist(
+    (set, get) => ({
+      products: [],
+
+      setProducts: (items) => set({ products: items }),
+
+      getProductById: (id) => get().products.find((p) => p.id === id),
+    }),
+    { name: "products" }
+  )
+);
